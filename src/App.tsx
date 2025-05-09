06ef4dfe6e0f88775072ed43fa0104c13336d84d
user <pranav19tikhe@gmail.com>
2025-05-09 18:26:50 +0530

    report sectio 2n

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from '@/components/ui/toaster';
 import { ReportFileProvider } from '@/ReportFileContext';
 
-// Pages
+// Pages this is route for the app
 import Index from '@/pages/Index';
 import Results from '@/pages/Results';
 import Reports from '@/pages/Reports';
